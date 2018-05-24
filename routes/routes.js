de52c1d8cd184ae502f026cb7c7f05dcc9230ca1
Sam Glerum <samuelglerum@hotmail.com>
2018-05-24 10:58:30 +0200

    added endpoint for registration

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,15 @@ const bodyParser = require('body-parser');
 
 let urlencodedParser = bodyParser.urlencoded({extended:false});
 
+// Endpoint for login page
 router.get('/', (req, res) => {
-   res.render("../views/login.ejs");
+   res.render('../views/login.ejs')
+});
+
+// Endpoint for registration
+router.get('/registration', (req, res) => {
+    console.log("test");
+    res.render('../views/register.ejs');
 });
 
 module.exports = router;
