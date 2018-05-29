e81d60da1a566639e388089af1c4461943290930
Rick Voesenek <rick.voesenek@hotmail.com>
2018-05-29 14:38:27 +0200

    Comments added

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -9,22 +9,31 @@ $(document).ready(function() {
 
         success: function (data, textStatus, xhr) {
             console.log("Succes");
+            // Fill firstname header in profile.ejs
             document.getElementById("lblvoornaam").innerHTML = data[0].firstname;
+            // Fill infix header in profile.ejs
             document.getElementById("lbltussenvoegsel").innerHTML = data[0].infix;
+            // Fill lastname header in profile.ejs
             document.getElementById("lblachternaam").innerHTML = data[0].lastname;
+            // Fill phonenumber header in profile.ejs
             document.getElementById("lbltelefoonnummer").innerHTML = data[0].phonenumber;
+            // Fill location header in profile.ejs
             document.getElementById("lbllocatie").innerHTML = data[0].job_location;
         },
         error: function (data, textStatus, error ) {
-            console.log(error + "ERROR!");
+            // Log the error
+            console.log(error);
         },
         complete: function(xhr, textStatus) {
+            // Log the status returned by the server
             console.log(xhr.status);
         }
     });
 });
 
 function setHeader(xhr) {
+    // Set Authorization header
     xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+    // Set X-Access-Token header
     xhr.setRequestHeader('X-Access-Token', token);
 }
