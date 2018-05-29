b5c8858cdc55d09beb3c3e74b73b07cdd2ac43dd
Sam Glerum <samuelglerum@hotmail.com>
2018-05-29 13:59:21 +0200

    fixed autofill of input fields

--- assets/js/changeprofile.js
+++ assets/js/changeprofile.js
@@ -1,4 +1,9 @@
 let token = getCookie("AuthToken");
+// let firstname = " ";
+// let lastname = " ";
+// let infix = " ";
+// let phonenumber = " ";
+// let location = " ";
 
 $(document).ready(function() {
     $.ajax({
@@ -6,13 +11,21 @@ $(document).ready(function() {
         url: 'https://mdod.herokuapp.com/api/psychologist',
         dataType: 'JSON',
         beforeSend: setHeader,
-        // headers: {
-        //     'X-Access-Token': token,
-        // },
 
         success: function (data, textStatus, xhr) {
             console.log("Succes");
             console.log(data[0].email);
+            // Fill firstname input field
+            document.getElementById("inputVoornaam").value = data[0].firstname;
+            // Fill infix if present
+            document.getElementById("inputTussenvoegsel").value = data[0].infix;
+            // Fill lastname input field
+            document.getElementById("inputAchternaam").value = data[0].lastname;
+            // Fill phonenumber input field
+            document.getElementById("inputTelefoonnummer").value = data[0].phonenumber;
+            // Fill location input field
+            document.getElementById("inputLocatie").value = data[0].job_location;
+
         },
         error: function (data, textStatus, error ) {
             console.log(error + "ERROR!");
