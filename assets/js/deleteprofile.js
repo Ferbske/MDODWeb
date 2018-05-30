8923dd7f63b56f9dc171f6d1bcbfd1d1e122159d
Kimmy :D <kimvg2@hotmail.com>
2018-05-30 12:49:20 +0200

    Delete profile werkt?
    Wachten op API bugfix

---
+++ assets/js/deleteprofile.js
@@ -0,0 +1,30 @@
+// let token = getCookie("AuthToken");
+
+function deletePsychologist() {
+    $.ajax({
+        type: 'DELETE',
+        url: 'https://mdod.herokuapp.com/api/psychologist',
+        dataType: 'JSON',
+        beforeSend: setHeader,
+
+        success: function (data, textStatus, xhr) {
+            alert("Account succesvol verwijderd, u wordt nu doorgestuurd naar de login pagina.");
+            window.location.href = '/login';
+        },
+        error: function (data, textStatus, error ) {
+            // Log the error
+            console.log("dit is een error " + error);
+        },
+        complete: function(xhr, textStatus) {
+            // Log the status returned by the server
+            console.log(xhr.status);
+        }
+    });
+};
+
+function setHeader(xhr) {
+    // Set Authorization header
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+    // Set X-Access-Token header
+    xhr.setRequestHeader('X-Access-Token', token);
+}
