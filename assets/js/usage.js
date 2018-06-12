0f31b17c65bb2e8a2abcfa76aadec6d40b9ebf80
Rick Voesenek <rick.voesenek@hotmail.com>
2018-06-12 10:04:40 +0200

    Bugfix Usage

--- assets/js/usage.js
+++ assets/js/usage.js
@@ -1,5 +1,4 @@
 let token = getCookie("AuthToken");
-let substances;
 
 function usageclient() {
     let email = getParameterByName("email");
@@ -23,11 +22,9 @@ function usageclient() {
         success: function (data, testStatus, xhr) {
             console.log("Succes");
             for (let x in data) {
-                let id = data[x].substanceId - 1;
-                let substance = substances[id].name;
                 txt += "<tr id='usage_data" + x + "'>" +
                     "<td class='usage_date'>" + data[x].usedAt.substring(0,10) + "</td>" +
-                    "<td class='usage_substance'>" + substance + "</td>" +
+                    "<td class='usage_substance'>" + data[x].name + "</td>" +
                     "<td class='usage_amount'>" + data[x].amount + "</td>" +
                     "<td class='usage_location'>" + data[x].location + "</td>" +
                     "<td class='usage_cause'>" + data[x].cause + "</td>" +
@@ -41,27 +38,6 @@ function usageclient() {
         error: function (data, textStatus, error) {
             console.log(error);
             document.getElementById("usage_body").innerHTML = txt;
-        },
-        complete: function (xhr, textStatus) {
-            console.log(xhr.status);
-        }
-    })
-}
-
-function allsubstances(){
-    $.ajax({
-        type: 'GET',
-        url: 'https://mdod.herokuapp.com/api/v1/substance/all',
-        dataType: 'JSON',
-        beforeSend: setHeader,
-
-        success: function (data, testStatus, xhr) {
-            console.log("Succes");
-            substances = data;
-            setTimeout(1000);
-        },
-        error: function (data, textStatus, error) {
-            console.log(error);
         },
         complete: function (xhr, textStatus) {
             console.log(xhr.status);
