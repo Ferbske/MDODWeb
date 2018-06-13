2c0fa1a340bdacb873d5f20a5dad4d91453185cb
Sam Glerum <samuelglerum@hotmail.com>
2018-06-13 11:45:12 +0200

    created function tableAllMessages which gets all messages from the database and puts them in a table. The table shows the sender, the message content and the send date

---
+++ assets/js/messages.js
@@ -0,0 +1,52 @@
+function tableAllMessages() {
+    let email = getParameterByName("email");
+    $.ajax({
+        type: 'POST',
+        url: 'https://mdod.herokuapp.com/api/v1/messages/get/psychologist',
+        dataType: 'JSON',
+        beforeSend: setHeader,
+        data: {
+            "email": email
+        },
+
+        success: function (data, testStatus, xhr) {
+            let txt = "";
+            console.log("Succes");
+
+            for (let x in data) {
+                txt += "<tr id='" + data[x].id + "' onclick=redirectupdatenote(" + data[x].id + ") >" +
+                    "<td class='messages_sender'>" + data[x].sendBy + "</td>" +
+                    "<td class='messages_message'>" + data[x].message + "</td>" +
+                    "<td class='messages_date'>" + data[x].date + "</td>" +
+                    "</tr>";
+            }
+            document.getElementById("tclients_messages").innerHTML = txt;
+        },
+        error: function (data, textStatus, error) {
+            console.log(error);
+            document.getElementById("tclients_messages").innerHTML = "";
+        },
+        complete: function (xhr, textStatus) {
+            console.log(xhr.status);
+        }
+    })
+}
+
+
+// Helper functions
+function setHeader(xhr) {
+    // Set Authorization header
+    let token = getCookie("AuthToken");
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+}
+
+// Get the parameter from the url
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
